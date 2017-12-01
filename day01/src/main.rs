cd8d5bb00d9be7005c7a5facde4c3dea880465d9
Michael Rutter <michael.john.rutter@gmail.com>
2017-12-01 18:43:59 +0100

    day1-part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,23 +1,40 @@
 extern crate failure;
 
 use std::io::{self, Read};
+use std::env;
 use std::process;
 use failure::Error;
 
 fn main() {
-    let mut parsed_input = parse_input().unwrap_or_else(|err| {
+    let parsed_input = parse_input().unwrap_or_else(|err| {
         eprintln!("Problem parsing input: {}", err);
         process::exit(1);
     });
 
-    if parsed_input.len() >= 3 {
-        let last = parsed_input[parsed_input.len() - 1];
-        parsed_input.insert(0, last);
+    let mut args = env::args();
+    args.next();
+
+    match args.next() {
+        Some(arg) => if arg == "2" {
+            part_two(parsed_input);
+        } else {
+            part_one(parsed_input);
+        },
+        None => {
+            part_one(parsed_input);
+        }
+    }
+}
+
+fn part_one(mut v: Vec<u32>) {
+    if v.len() >= 3 {
+        let last = v[v.len() - 1];
+        v.insert(0, last);
     }
 
     let mut sum = 0;
 
-    let mut p = parsed_input.iter().peekable();
+    let mut p = v.iter().peekable();
 
     loop {
         let num = match p.next() {
@@ -34,6 +51,26 @@ fn main() {
     }
 
     println!("{:?}", sum);
+}
+
+fn part_two(v: Vec<u32>) {
+    let mut sum = 0;
+    let length = v.len();
+    let distance = length / 2;
+
+    for (index, elem) in v.iter().enumerate() {
+        let mut pair_index = index + distance;
+
+        if pair_index > length - 1 {
+            pair_index = pair_index - length;
+        }
+
+        if &v[pair_index] == elem {
+            sum = sum + elem;
+        }
+    }
+
+    println!("{}", sum);
 }
 
 fn parse_input() -> Result<Vec<u32>, Error> {
