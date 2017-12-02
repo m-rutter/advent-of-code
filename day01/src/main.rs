a909bd2964d5e44c97d7b5cea3baaf8b911522c1
Michael Rutter <michael.john.rutter@gmail.com>
2017-12-02 16:05:11 +0100

    tidy up

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,11 +10,59 @@ fn main() {
         process::exit(1);
     });
 
-    let part_one = check_pairs(&parsed_input, 1);
+
+
+    part_one(&parsed_input);
     let part_two = check_pairs(&parsed_input, &parsed_input.len() / 2);
 
-    println!("Part one solution: {}", part_one);
     println!("Part two solution: {}", part_two);
+}
+
+fn part_one(v: &Vec<u32>) {
+    let mut v = v.clone();
+    let length = v.len();
+
+    if length >= 3 {
+        if let Some(&a) = v.last() {
+            v.insert(0, a);
+        }
+    }
+
+    let mut sum = 0;
+
+    let mut p = v.iter().peekable();
+
+    loop {
+        let num = match p.next() {
+            Some(n) => n,
+            None => break,
+        };
+
+        match p.peek() {
+            Some(&next_num) => if num == next_num {
+                sum = num + sum;
+            },
+            None => break,
+        }
+    }
+
+    println!("Part one solution: {}", sum);
+}
+
+fn check_pairs(v: &Vec<u32>, look_ahead: usize) -> u32 {
+    let mut sum = 0;
+    let length = v.len();
+
+    for (index, elem) in v.iter().enumerate() {
+        let pair_index = index as i32 + look_ahead as i32;
+        let pair_index = (pair_index - length as i32).abs() as usize;
+
+        if &v[pair_index] == elem {
+            sum = sum + elem;
+        }
+    }
+
+    sum
 }
 
 fn parse_input() -> Result<Vec<u32>, Error> {
@@ -27,19 +75,4 @@ fn parse_input() -> Result<Vec<u32>, Error> {
     let v: Vec<u32> = buff.chars().filter_map(|c| c.to_digit(10)).collect();
 
     Ok(v)
-}
-
-fn check_pairs(v: &Vec<u32>, look_ahead: usize) -> u32 {
-    let mut sum = 0;
-    let length = v.len();
-
-    for (index, elem) in v.iter().enumerate() {
-        let pair_index: usize = ((index + look_ahead) as i32 - length as i32).abs() as usize;
-
-        if &v[pair_index] == elem {
-            sum = sum + elem;
-        }
-    }
-
-    sum
 }
