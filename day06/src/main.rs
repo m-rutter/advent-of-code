83aeedafc580831b189e777387af595093d8c9c9
Michael Rutter <michael.john.rutter@gmail.com>
2017-12-28 22:37:59 +0000

    day 06 part 1 and 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,6 +3,7 @@ extern crate failure;
 use failure::Error;
 use std::io::{self, Read};
 use std::process;
+use std::collections::HashMap;
 
 
 fn main() {
@@ -11,9 +12,67 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{:?}", input)
+    let part_one = relocate_until_repeat(&input);
+
+    println!("Part one solution is: {}", part_one.0);
+    println!("Part two solution is: {}", part_one.1);
 }
 
+fn relocate_until_repeat(banks: &Vec<u32>) -> (u32, u32) {
+    let mut set = HashMap::new();
+    let loop_size;
+    set.insert(banks.clone(), 0);
+
+    let mut banks = banks.clone();
+
+    let mut cycles = 0;
+
+    loop {
+        cycles += 1;
+
+        let max_bank_index = banks
+            .iter()
+            .max()
+            .and_then(|max| {
+                banks
+                    .iter()
+                    .enumerate()
+                    .find(|&(_, bank_blocks)| bank_blocks == max)
+            })
+            .expect("There shouldn't an empty vector")
+            .0;
+
+        cycle(&mut banks, max_bank_index);
+
+        if set.contains_key(&banks) {
+            loop_size = cycles - set.get(&banks).unwrap();
+
+            break;
+        }
+
+        set.insert(banks.clone(), cycles);
+    }
+
+    (cycles, loop_size)
+}
+
+fn cycle(banks: &mut Vec<u32>, start: usize) {
+    let length = banks.len();
+    let mut blocks = banks[start];
+    banks[start] = 0;
+    let mut current = start;
+
+    while blocks > 0 {
+        current += 1;
+
+        if current == length {
+            current = 0
+        };
+
+        banks[current] += 1;
+        blocks -= 1;
+    }
+}
 
 fn parser() -> Result<Vec<u32>, Error> {
     let mut stdin = io::stdin();
@@ -22,7 +81,8 @@ fn parser() -> Result<Vec<u32>, Error> {
 
     stdin.read_to_string(&mut buff)?;
 
-    let banks = buff.split_whitespace()
+    let banks = buff.trim()
+        .split_whitespace()
         .filter_map(|bank| bank.parse().ok())
         .collect();
 
