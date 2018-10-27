10d45c76729bec98c35fe08d7318973ebc2cdbeb
Michael Rutter <michael.john.rutter@gmail.com>
2018-10-27 14:18:20 +0100

    deleted eronous empty file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 
 fn main() {
     let matches = App::new("Advent of Code 2017 Solver in Rust")
-        .version("0.6.0")
+        .version("0.1.0")
         .author("Michael Rutter <michael.john.rutter@gmail.com>")
         .about("Solves 2017 Advent of Code problems")
         .arg(
