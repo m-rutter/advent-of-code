5ad561ed72732f844e2a91b1ed18ca26ed2cca99
Michael Rutter <michael.john.rutter@gmail.com>
2018-12-13 20:23:36 +0000

    partially completed day 3 - something is still wrong

--- src/aoc2018/day03.rs
+++ src/aoc2018/day03.rs
@@ -1,6 +1,7 @@
 use crate::{error, AoCSolution};
 use pest::{self, Parser};
 use pest_derive::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 #[grammar = "aoc2018/day03.pest"]
@@ -18,12 +19,34 @@ struct Claim {
 pub fn run(input: &str) -> error::AoCResult<AoCSolution> {
     let claims = parse(&input);
 
-    println!("{:?}", claims);
-
     Ok(AoCSolution {
-        part_one: String::new(),
+        part_one: count_overlapping_claims(&claims).to_string(),
         part_two: String::new(),
     })
+}
+
+fn count_overlapping_claims(claims: &[Claim]) -> u32 {
+    let mut cloth: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut count = 0;
+
+    for claim in claims.iter() {
+        let x_range = claim.left..(claim.width + claim.left);
+        let y_range = claim.top..(claim.height + claim.top);
+
+        for x in x_range {
+            for y in y_range.clone() {
+                let value = cloth.entry((x, y)).or_insert(0);
+
+                *value += 1;
+
+                if *value == 2 {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
 }
 
 fn parse(input: &str) -> Vec<Claim> {
@@ -35,8 +58,8 @@ fn parse(input: &str) -> Vec<Claim> {
                 if let Rule::record = record.as_rule() {
                     let mut record_pair = record.into_inner();
 
-                    // if the pest libary is to be believed, it is literately
-                    // impossible for these to be Result::Err at this point
+                    // if the pest libary is to be believed, it is impossible
+                    // for these Results to be Result::Err at this point
                     claims.push(Claim {
                         id: record_pair.next().unwrap().as_str().parse().unwrap(),
                         left: record_pair.next().unwrap().as_str().parse().unwrap(),
@@ -50,4 +73,21 @@ fn parse(input: &str) -> Vec<Claim> {
     }
 
     claims
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn correct_result_original_example() {
+        let s = r#"
+        #1 @ 1,3: 4x4
+        #2 @ 3,1: 4x4
+        #3 @ 5,5: 2x2
+        "#;
+
+        let c = parse(s);
+
+        assert_eq!(count_overlapping_claims(&c), 4);
+    }
 }
