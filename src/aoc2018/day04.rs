0ad77c688c1a06bbc383b448b8d3537c71fd358c
Michael Rutter <michael.john.rutter@gmail.com>
2018-12-14 08:53:03 +0000

    started day04 2018

---
+++ src/aoc2018/day04.rs
@@ -0,0 +1,37 @@
+use chrono::{NaiveTime};
+use pest::{self, Parser};
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammer = "aoc2018/day04.pest"]
+struct Day04Parser
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn original_example() {
+
+        let input = r#"
+        [1518-11-01 00:00] Guard #10 begins shift
+        [1518-11-01 00:05] falls asleep
+        [1518-11-01 00:25] wakes up
+        [1518-11-01 00:30] falls asleep
+        [1518-11-01 00:55] wakes up
+        [1518-11-01 23:58] Guard #99 begins shift
+        [1518-11-02 00:40] falls asleep
+        [1518-11-02 00:50] wakes up
+        [1518-11-03 00:05] Guard #10 begins shift
+        [1518-11-03 00:24] falls asleep
+        [1518-11-03 00:29] wakes up
+        [1518-11-04 00:02] Guard #99 begins shift
+        [1518-11-04 00:36] falls asleep
+        [1518-11-04 00:46] wakes up
+        [1518-11-05 00:03] Guard #99 begins shift
+        [1518-11-05 00:45] falls asleep
+        [1518-11-05 00:55] wakes up
+        "#
+
+        
+        unimplemented!();
+    }
+}
