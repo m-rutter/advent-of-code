de81cd39eb302f7d8c2170c1ac26a0c182a3237b
Michael Rutter <michael.john.rutter@gmail.com>
2018-12-17 08:40:37 +0000

    too tried

--- src/aoc2018/day06.rs
+++ src/aoc2018/day06.rs
@@ -1,10 +1,64 @@
 use crate::{error, AoCSolution};
 use failure::{format_err, Error};
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::str::FromStr;
 
 pub fn run(input: &str) -> error::AoCResult<AoCSolution> {
     let coordinates = parse(&input);
+
+    let upper_bound = 1 + coordinates
+        .iter()
+        .map(|c| if c.x > c.y { c.x } else { c.y })
+        .max()
+        .unwrap() as usize;
+
+    let mut grid = vec![vec![0 as usize; upper_bound]; upper_bound];
+
+    let range = 0..upper_bound;
+
+    for x in range.clone() {
+        for y in range.clone() {
+            let distances: Vec<(usize, i32)> = coordinates
+                .iter()
+                .enumerate()
+                .map(|(i, c)| (i, c.distince_from((x as i32, y as i32))))
+                .collect();
+
+            let shortest = distances.iter().min_by(|a, b| a.1.cmp(&b.1)).unwrap();
+
+            grid[x][y] = shortest.0;
+        }
+    }
+
+    let coordinates_to_ignore: HashSet<(usize, usize)> =
+        range.clone().fold(HashSet::new(), |mut acc, i| {
+            acc.insert((0, i));
+            acc.insert((i, 0));
+            acc.insert((upper_bound - 1, i));
+            acc.insert((i, upper_bound - 1));
+
+            acc
+        });
+
+    let mut regions = vec![0; coordinates.len()];
+    for x in range.clone() {
+        for y in range.clone() {
+            let point = grid[x][y];
+
+            if !coordinates_to_ignore.contains(&(x, y)) {
+                regions[point] += 1
+            }
+        }
+    }
+
+    let largest_area = regions
+        .iter()
+        .enumerate()
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .unwrap()
+        .1;
+
+    println!("{}", largest_area);
 
     unimplemented!()
 }
@@ -12,85 +66,18 @@ pub fn run(input: &str) -> error::AoCResult<AoCSolution> {
 fn parse(input: &str) -> Vec<Coordinates> {
     input
         .lines()
-        .enumerate()
-        .filter_map(|(id, line)| {
-            let c = Coordinates::from_str(&line).ok();
-
-            match c {
-                Some(mut coordinates) => {
-                    coordinates.id = Some(id as i32);
-                    Some(coordinates)
-                }
-                None => None,
-            }
-        })
+        .filter_map(|line| Coordinates::from_str(&line).ok())
         .collect()
 }
 
-struct Grid<'a> {
-    width: i32,
-    height: i32,
-    points: &'a [Coordinates],
-    grid: HashMap<&'a Coordinates, ID>,
-}
-
-type ID = i32;
-
-impl<'a> From<&'a [Coordinates]> for Grid<'a> {
-    fn from(v: &'a [Coordinates]) -> Grid<'a> {
-        let w = v.iter().map(|c| c.x).max_by(|a, b| a.cmp(&b)).unwrap_or(0);
-        let h = v.iter().map(|c| c.y).max_by(|a, b| a.cmp(&b)).unwrap_or(0);
-
-        let grid = HashMap::with_capacity((w * h) as usize);
-
-        let x_range = 0..w;
-        let y_range = 0..h;
-
-        for x in x_range {
-            for y in y_range.clone() {
-                let c = Coordinates::new(x, y);
-                let closest = c.find_closest(&v);
-            }
-        }
-
-        Grid {
-            width: w,
-            height: h,
-            points: v,
-            grid: grid,
-        }
-    }
-}
-
-#[derive(Hash, Eq, PartialEq, Debug)]
 struct Coordinates {
-    id: Option<i32>,
     x: i32,
     y: i32,
 }
 
 impl Coordinates {
-    fn new(x: i32, y: i32) -> Self {
-        Coordinates {
-            x: x,
-            y: y,
-            id: None,
-        }
-    }
-
-    fn distince_from(&self, other: &Coordinates) -> i32 {
-        (self.x - other.x).abs() + (self.y - other.y).abs()
-    }
-
-    fn find_closest(&self, v: &[Coordinates]) -> Option<i32> {
-        match v
-            .into_iter()
-            .map(|c| (self.distince_from(c), c))
-            .min_by(|a, b| a.0.cmp(&b.0))
-        {
-            Some((_, c)) => c.id,
-            None => None,
-        }
+    fn distince_from(&self, other: (i32, i32)) -> i32 {
+        (self.x - other.0).abs() + (self.y - other.0).abs()
     }
 }
 
@@ -103,11 +90,7 @@ impl FromStr for Coordinates {
         let x = v.next().ok_or(format_err!("No x coordinate"))?;
         let y = v.next().ok_or(format_err!("No y coordinate"))?;
 
-        Ok(Coordinates {
-            id: None,
-            x: x,
-            y: y,
-        })
+        Ok(Coordinates { x: x, y: y })
     }
 }
 
