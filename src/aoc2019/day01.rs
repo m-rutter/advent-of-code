a361e365880939aa3d3e2d829c4169bf7c19b04c
Michael Rutter <michael.john.rutter@gmail.com>
2019-12-02 00:22:37 +0000

    day 04 part 2

--- src/aoc2019/day01.rs
+++ src/aoc2019/day01.rs
@@ -5,28 +5,44 @@ pub fn run(input: &str) -> error::AoCResult<Solution> {
     let parsed_input = parse(input);
 
     Ok(Solution {
-        part_one: part_one(&parsed_input).to_string(),
-        part_two: String::new(),
+        part_one: part_one(&parsed_input).iter().sum::<Module>().to_string(),
+        part_two: part_two(&parsed_input).to_string(),
     })
 }
 
-type Modules = Vec<f64>;
+type Module = i64;
 
-fn parse(input: &str) -> Modules {
+fn parse(input: &str) -> Vec<Module> {
     input
         .lines()
         .filter_map(|line| line.trim().parse().ok())
         .collect()
 }
 
-fn part_one(modules: &Modules) -> f64 {
-    modules
-        .iter()
-        .map(|module| (module / 3.0).floor() - 2.0)
-        .sum()
+fn fuel_calc(module: &Module) -> Module {
+    module / 3 - 2
 }
 
-fn part_two(modules: &Modules) {}
+fn part_one(modules: &[Module]) -> Vec<Module> {
+    modules.iter().map(fuel_calc).collect()
+}
+
+fn part_two(modules: &[Module]) -> Module {
+    part_one(modules)
+        .iter()
+        .map(|fuel_mass| {
+            let mut fuel = *fuel_mass;
+            let mut fuel_to_add = *fuel_mass;
+            loop {
+                fuel_to_add = fuel_calc(&fuel_to_add);
+                if fuel_to_add <= 0 {
+                    break fuel;
+                }
+                fuel += fuel_to_add;
+            }
+        })
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -38,7 +54,7 @@ mod tests {
         let result = run(&input).unwrap();
 
         assert_eq!(result.part_one, "3297626");
-        assert_eq!(result.part_two, "");
+        assert_eq!(result.part_two, "4943578");
     }
 
     #[test]
@@ -53,7 +69,9 @@ mod tests {
         let parsed_input = parse(input);
 
         let part_one_solution = part_one(&parsed_input);
+        let part_two_solution = part_two(&[100756]);
 
-        assert_eq!(part_one_solution, 34241 as f64);
+        assert_eq!(part_one_solution.iter().sum::<Module>(), 34241);
+        assert_eq!(part_two_solution, 50346);
     }
 }
