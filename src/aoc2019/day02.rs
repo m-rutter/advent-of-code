2f77ce20ad7bf6a4b7ca89d155e441aff37b9be5
Michael Rutter <michael.john.rutter@gmail.com>
2019-12-03 13:17:37 +0000

    override removing index accesses

--- src/aoc2019/day02.rs
+++ src/aoc2019/day02.rs
@@ -14,7 +14,7 @@ pub fn run(input: &str) -> error::AoCResult<Solution> {
     ops[1] = 12;
     ops[2] = 2;
 
-    let part_one = execute_intcode(&mut ops)?;
+    let part_one = IntCodeExecutor::new(&ops).execute()?;
 
     Ok(Solution {
         part_one: part_one.to_string(),
@@ -29,30 +29,75 @@ fn parse(input: &str) -> Vec<usize> {
         .collect()
 }
 
-fn execute_intcode(ops: &[usize]) -> error::AoCResult<usize> {
-    let mut registers = Vec::from(ops);
-    let mut cursor: usize = 0;
+struct IntCodeExecutor {
+    registers: Vec<usize>,
+}
 
-    loop {
-        if cursor > registers.len() - 1 {
-            Err(error::Error::msg(&"No opcode provided"))?;
+impl IntCodeExecutor {
+    fn new(registers: &[usize]) -> Self {
+        IntCodeExecutor {
+            registers: Vec::from(registers),
         }
+    }
 
-        let op = Op::try_from(&registers[cursor..])?;
+    fn execute(mut self) -> error::AoCResult<usize> {
+        let mut cursor: usize = 0;
 
-        match op {
-            Op::Add(param1, param2, output) => {
-                let value = registers[param1] + registers[param2];
-                registers[output] = value;
+        loop {
+            if cursor > self.registers.len() - 1 {
+                Err(error::Error::msg(&"No opcode provided"))?;
             }
-            Op::Multiply(param1, param2, output) => {
-                let value = registers[param1] * registers[param2];
-                registers[output] = value;
+
+            let op = Op::try_from(&self.registers[cursor..])?;
+
+            match op {
+                Op::Add(param1, param2, output) => {
+                    let value = if let (Some(x), Some(y)) =
+                        (self.registers.get(param1), self.registers.get(param2))
+                    {
+                        x + y
+                    } else {
+                        Err(error::Error::msg(&format!(
+                            "Invalid indexes in op code for parameters (out of bounds): {}, {}",
+                            param1, param2,
+                        )))?
+                    };
+
+                    if let Some(elem) = self.registers.get_mut(output) {
+                        *elem = value
+                    } else {
+                        Err(error::Error::msg(&format!(
+                            "Invalid index in op code for output (out of bounds): {}",
+                            output
+                        )))?
+                    };
+                }
+                Op::Multiply(param1, param2, output) => {
+                    let value = if let (Some(x), Some(y)) =
+                        (self.registers.get(param1), self.registers.get(param2))
+                    {
+                        x * y
+                    } else {
+                        Err(error::Error::msg(&format!(
+                            "Invalid indexes in op code for parameters (out of bounds): {}, {}",
+                            param1, param2,
+                        )))?
+                    };
+
+                    if let Some(elem) = self.registers.get_mut(output) {
+                        *elem = value
+                    } else {
+                        Err(error::Error::msg(&format!(
+                            "Invalid index in op code for output (out of bounds): {}",
+                            output
+                        )))?
+                    };
+                }
+                Op::Terminal => break Ok(self.registers[0]),
             }
-            Op::Terminal => break Ok(registers[0]),
+
+            cursor += 4;
         }
-
-        cursor += 4;
     }
 }
 
@@ -98,7 +143,9 @@ mod tests {
 
         let ops = parse(input);
 
-        let code = execute_intcode(&ops).expect("Error unwrapping op");
+        let code = IntCodeExecutor::new(&ops)
+            .execute()
+            .expect("Error unwrapping op");
 
         assert_eq!(30, code);
     }
@@ -109,7 +156,9 @@ mod tests {
 
         let ops2 = parse(input2);
 
-        let code2 = execute_intcode(&ops2).expect("Error unwrapping op");
+        let code2 = IntCodeExecutor::new(&ops2)
+            .execute()
+            .expect("Error unwrapping op");
 
         assert_eq!(3500, code2);
     }
@@ -118,8 +167,8 @@ mod tests {
     fn matches_offical_result() {
         let input = include_str!("./input/day02");
 
-        let x = 2 >= 3;
-
         let result = run(&input).unwrap();
+
+        assert_eq!(result.part_one, "3716250");
     }
 }
