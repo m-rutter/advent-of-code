8ee228d645f75b08c42885110c5ec8bca55ed39d
Michael Rutter <michael.rutter@protonmail.com>
2025-12-01 20:00:04 +0000

    day 1 part 1

--- aoc/src/aoc2025/day01.rs
+++ aoc/src/aoc2025/day01.rs
@@ -1,7 +1,74 @@
-use crate::{Solution, error::Result};
+use std::str::FromStr;
+
+use anyhow::anyhow;
+
+use crate::{
+    Solution,
+    error::{AoCError, Result},
+};
 
 pub fn run(input: &str) -> Result<Solution> {
-    unimplemented!()
+    let instructions: Vec<Instruction> = input
+        .trim()
+        .lines()
+        .map(|line| line.trim().parse())
+        .collect::<Result<_>>()?;
+
+    let mut pos = 50;
+    let mut zero_pos_count = 0;
+    for instruction in instructions.iter() {
+        match instruction.direction {
+            Direction::R => {
+                pos = (pos + instruction.turn) % 100;
+            }
+            Direction::L => {
+                pos = (100 + pos - instruction.turn % 100) % 100;
+            }
+        }
+
+        if pos == 0 {
+            zero_pos_count += 1;
+        }
+    }
+
+    Ok(Solution {
+        part_one: zero_pos_count.to_string(),
+        part_two: "".to_string(),
+    })
+}
+
+#[derive(Debug)]
+struct Instruction {
+    direction: Direction,
+    turn: i32,
+}
+
+#[derive(Debug)]
+enum Direction {
+    L,
+    R,
+}
+
+impl FromStr for Instruction {
+    type Err = AoCError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let direction = chars.next();
+        let movement: i32 = String::from_iter(chars).parse()?;
+
+        Ok(match direction {
+            Some('L') => Self {
+                direction: Direction::L,
+                turn: movement,
+            },
+            Some('R') => Self {
+                direction: Direction::R,
+                turn: movement,
+            },
+            _ => return Err(anyhow!("expected 'L' or 'R' char at start of each line").into()),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -10,12 +77,35 @@ mod tests {
 
     #[test]
     fn test_offical_example_1() {
-        let input = r"";
+        let input = r"L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82";
 
         let result = run(input).unwrap();
 
-        assert_eq!(result.part_one, "");
-        assert_eq!(result.part_two, "");
+        assert_eq!(result.part_one, "3");
+        // assert_eq!(result.part_two, "6");
+    }
+
+    #[test]
+    fn test_zoey_example() {
+        let input = r"L50
+        R200
+        L50
+        R100
+        L175";
+
+        let result = run(input).unwrap();
+
+        assert_eq!(result.part_one, "2");
+        // assert_eq!(result.part_two, "6");
     }
 
     #[test]
@@ -24,7 +114,7 @@ mod tests {
 
         let solution = run(&input).unwrap();
 
-        assert_eq!(solution.part_one, "");
-        assert_eq!(solution.part_two, "");
+        assert_eq!(solution.part_one, "989");
+        // assert_eq!(result.part_two, "");
     }
 }
