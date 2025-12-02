d924a3f64017f19e6397ba39472cba6a8a3f9897
Michael Rutter <michael.rutter@protonmail.com>
2025-12-02 12:27:04 +0000

    setup day 2

---
+++ aoc/src/aoc2025/day02.rs
@@ -0,0 +1,16 @@
+use crate::{Solution, error::Result};
+
+pub fn run(input: &str) -> Result<Solution> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_offical_example_1() {}
+
+    #[test]
+    fn test_offical_input() {}
+}
--- aoc/src/aoc2025/mod.rs
+++ aoc/src/aoc2025/mod.rs
@@ -1,10 +1,12 @@
 pub mod day01;
+pub mod day02;
 
 use crate::{Config, Solution, error};
 
 pub fn run(config: &Config) -> error::Result<Solution> {
     match config.day {
         1 => day01::run(&config.input),
+        2 => day02::run(&config.input),
         _ => Err(error::AoCError::UnsupportedDay {
             day: config.day,
             year: config.year,
